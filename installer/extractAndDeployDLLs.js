33987b97ed0e65050f4deea3c02aae37fbca4519
trepichio <trepichio@gmail.com>
2019-12-18 16:09:49 -0300

    extract and deploy dlls with pkg

--- installer/extractAndDeployDLLs.js
+++ installer/extractAndDeployDLLs.js
@@ -4,9 +4,12 @@ const arch = require('arch')
 const AdmZip = require('adm-zip');
 
 module.exports = () => {
+  //* where to get compressed file with DLLs from
   const rootDir = "C:\\MBD\\Install\\"
+  //* path to compressed file
   const zipFile = path.join(rootDir, "Dll's (_CAT_ & _Instal_).zip")
   console.log("TCL: rar", zipFile)
+  //* path destination where compressed files should be extracted
   const zipDest = path.resolve(`${rootDir}`)
 
 
@@ -15,56 +18,58 @@ module.exports = () => {
   var [dllExtractedFolder] = zip.getEntries() // an array of ZipEntry records
   console.log("TCL: main -> dllExtractedFolder", dllExtractedFolder.entryName)
 
+  //* checking if 7z is installed
   console.log(shell.which('7z'))
   const extractSh = shell.exec(`7z.exe e "${zipFile}" -o${zipDest} -r -y -spf`)
+
   if (extractSh.code === 0) {
+    //* successful!
     console.log(extractSh.stdout);
     console.log("DLL Files extracted successfully.")
 
+    //* copy DLLs from original/source folder to Windows folder according to PC Architecture (x86 or x64)
     const src = path.join(rootDir, dllExtractedFolder.entryName)
-    const dest = {
-      sys32: path.resolve(`C:\\Windows\\System32`),
-      sys64: path.resolve(`C:\\Windows\\SysWOW64`)
-    }
-    console.log("TCL: src", src)
-    console.log("TCL: dest", dest)
+    const sysPath = { x86: ['System32'], x64: ['System32', 'SysWOW64'] }
+    const paths = sysPath[arch()]
+
     console.log("Copying DLLs to System32 and SysWOW64")
+    console.log("TCL: src", src)
+    console.log("TCL: dest", paths)
 
-    const copyx86Sh = shell.exec(`xcopy "${src}\\*.*" ${dest.sys32} /f /y`)
+    for (const path of paths) {
+      console.log("TCL Copy DLL: path", path)
+      let copySh = shell.exec(`xcopy "${src}\\*.*" C:\\Windows\\${path} /f /y`)
+      copySh.code !== 0 && console.log(`Copy to ${path} failed. Error:  ${copySh.stderr}`);
 
-    copyx86Sh.code !== 0 && console.log("Copy to System32 failed. Error:", copyx86Sh.stderr);
 
-    const copyx64Sh = shell.exec(`xcopy "${src}\\*.*" ${dest.sys64} /f /y`)
+      if (shell.exec(`Echo Get-ExecutionPolicy | PowerShell.exe -noprofile -`).stdout.match('Restricted')) {
+        console.log(shell.exec(`Echo Set-ExecutionPolicy Unrestricted | PowerShell.exe -noprofile -`).stdout)
+      }
 
-    copyx64Sh.code !== 0 && console.log("Copy to SysWOW64 failed. Error:", copyx64Sh.stderr);
+      //* register DLLs from original/source folder to Windows folder according to PC Architecture (x86 or x64)
+      registerDLLs(path)
+    }
 
+    // remove original/source extracted folder and leave compressed file at same place
     shell.rm("-Rf", src)
     console.log(`Removed ${src}`)
-
-    registerDLLs()
   }
   else {
     console.log(extractSh.stderr);
     console.log("DLL Files extraction failed. Do it yourself and don't forget to register them!! ")
-
   }
 
-  function registerDLLs() {
+  function registerDLLs(path) {
+    console.log("TCL: registerDLLs -> path", path)
 
-    const sysPath = { x86: ['System32'], x64: ['System32', 'SysWOW64'] }
-    const paths = sysPath[arch()]
-    for (const path of paths) {
-      console.log("TCL: registerDLLs -> path", path)
+    shell.exec(`regsvr32 %windir%\\${path}\\capicom.dll /s`) && console.log("registering capicom.dll");
 
-      shell.exec(`regsvr32 %windir%\\${path}\\capicom.dll /s`) && console.log("registering capicom.dll");
+    shell.exec(`regsvr32 %windir%\\${path}\\msxml5.dll /s`) && console.log("registering msxml5.dll");
 
-      shell.exec(`regsvr32 %windir%\\${path}\\msxml5.dll /s`) && console.log("registering msxml5.dll");
+    shell.exec(`regsvr32 %windir%\\${path}\\ssleay32.dll /s`) && console.log("registering ssleay32.dll");
 
-      shell.exec(`regsvr32 %windir%\\${path}\\ssleay32.dll /s`) && console.log("registering ssleay32.dll");
+    shell.exec(`regsvr32 %windir%\\${path}\\libeay32.dll /s`) && console.log("registering libeay32.dll");
 
-      shell.exec(`regsvr32 %windir%\\${path}\\libeay32.dll /s`) && console.log("registering libeay32.dll");
-
-      shell.exec(`regsvr32 %windir%\\${path}\\midas.dll /s`) && console.log("registering midas.dll");
-    }
+    shell.exec(`regsvr32 %windir%\\${path}\\midas.dll /s`) && console.log("registering midas.dll");
   }
 }

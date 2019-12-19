401ead392ba5eaebcdf1190402a35613fe2d3799
trepichio <trepichio@gmail.com>
2019-12-19 18:57:13 -0300

    [NEED TEST] added require('os').homedir()

--- installer/deployDesktopShortcuts.js
+++ installer/deployDesktopShortcuts.js
@@ -1,17 +1,28 @@
 const ws = require("windows-shortcuts")
 const path = require("path")
+// const sleep = require("./helpers/sleep")
+// const apps = [
+//   { parentFolder: "Sistemas/Retaguarda", name: "Retaguarda", execPath: "Retaguarda.exe" },
+//   { parentFolder: "Sistemas/Pre-venda", name: "Pre-venda", execPath: "PreVenda.exe" },
+//   { parentFolder: "Sistemas/Frente", name: "Frente", execPath: "Caixa.exe" },
+//   { parentFolder: "Sistemas/SAT Gerenciador", name: "SAT Gerenciador", execPath: "SATGerenciador.exe" },
+//   { parentFolder: "Acesso", name: "AnyDesk", execPath: "AnyDesk.exe" },
+//   { parentFolder: "Acesso", name: "TeamViewer", execPath: "TeamViewerQS.exe" }
+// ]
 const apps = [
-  { parentFolder: "Sistemas/Retaguarda", name: "Retaguarda", execPath: "Retaguarda.exe" },
-  { parentFolder: "Sistemas/Pre-venda", name: "Pre-venda", execPath: "PreVenda.exe" },
-  { parentFolder: "Sistemas/Frente", name: "Frente", execPath: "Caixa.exe" },
-  { parentFolder: "Sistemas/SAT Gerenciador", name: "SAT Gerenciador", execPath: "SATGerenciador.exe" },
-  { parentFolder: "Acesso", name: "AnyDesk", execPath: "AnyDesk.exe" },
-  { parentFolder: "Acesso", name: "TeamViewer", execPath: "TeamViewerQS.exe" }
+  { parentFolder: "Retaguarda", name: "Retaguarda", execPath: "Retaguarda.exe" },
+  { parentFolder: "Frente Touch", name: "Frente Touch", execPath: "FrenteTouch.exe" },
+  { parentFolder: "Frente", name: "Frente", execPath: "Caixa.exe" },
+  { parentFolder: "Sat Gerenciador", name: "SAT Gerenciador", execPath: "SATGerenciador.exe" },
 ]
-const rootDir = path.join(process.cwd(), "C:/MBD")
-const main = () => {
+const rootDir = path.resolve("C:/MBD")
+console.log("TCL: rootDir", rootDir)
+module.exports = () => {
+  const homedir = require('os').homedir();
+  console.log("TCL: main -> homedir", homedir)
   for (app of apps) {
-    let shortcut = path.join((process.execPath), `%UserProfile%/Desktop/${app.name}.lnk`)
+    // let shortcut = path.join((process.execPath), `%UserProfile%/Desktop/${app.name}.lnk`)
+    let shortcut = path.join(homedir, `Desktop/${app.name}.lnk`)
     console.log("TCL: main -> shortcut", shortcut)
     ws.create(
       shortcut,
@@ -21,13 +32,13 @@ const main = () => {
         icon: `${rootDir}/${app.parentFolder}/${app.execPath}`,
         runStyle: ws.MAX,
       }, (err) => {
-        if (err)
+        if (err) {
           throw Error(err);
-        else
-          console.log(`Shortcut of ${app.name} created!`);
+        }
+        else {
+          console.log(`Shortcut of ${shortcut} created!`);
+        }
       }
     )
   }
 }
-
-main()
